5a0ac043936ca8dc8e6ddfc19887126635d2d151
Draganitchki <47672318+Draganitchki@users.noreply.github.com>
2019-02-17 22:53:30 -0500

    Add files via upload

---
+++ spot.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicpuzzle;
+
+/**
+ *
+ * @author Aaron
+ */
+public class spot {
+    
+    private boolean selection;
+    
+    public spot(){
+        selection = false;
+    }
+    
+    public spot(boolean selection){
+        this.selection = selection;
+    }
+}
